b4dc46e04aa5459fcd8a44c01b40b0a8f0a9080d
Deian Mishev <d.s.mishev@gmail.com>
2021-02-01 00:32:49 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -56,6 +56,7 @@ np.random.seed(seedInt)
 np.random.shuffle(train_labels)
 
 # Vectorizing Data and Labels
+# TODO: Change one hot encoding for word embedding
 x_train = vectorize_sequences(train_data, modelWordsNumber)
 y_train = np.asarray(train_labels).astype('float32')
 
